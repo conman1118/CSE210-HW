9693df5d473cb433e1b9885d383051e79a0e50b1
conman1118 <connor.f.neus@gmail.com>
2025-05-14 16:50:55 -0700

    changed a few things

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -6,15 +6,15 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
-        job1._jobTitle = "Software Engineer";
-        job1._company = "Microsfot";
-        job1._startYear = 2019;
-        job1._endYear = 2020;
+        job1._jobTitle = "Property Manager";
+        job1._company = "Pink Door Storage";
+        job1._startYear = 2024;
+        job1._endYear = 2025;
 
         Job job2 = new Job();
         job2._jobTitle = "Manager";
-        job2._company = "Apple";
-        job2._startYear = 2022;
-        job2._endYear = 2023;
+        job2._company = "Fry's";
+        job2._startYear = 2021;
+        job2._endYear = 2022;
     }
 }
