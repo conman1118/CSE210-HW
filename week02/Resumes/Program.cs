8403f6a85100c08e64b7a6095fbfcb951a6aea04
conman1118 <connor.f.neus@gmail.com>
2025-05-15 11:01:54 -0700

    all done

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -10,11 +10,22 @@ class Program
         job1._company = "Pink Door Storage";
         job1._startYear = 2024;
         job1._endYear = 2025;
+        job1.Display();
 
         Job job2 = new Job();
         job2._jobTitle = "Manager";
         job2._company = "Fry's";
         job2._startYear = 2021;
         job2._endYear = 2022;
+        job2.Display();
+
+
+        Resume resume = new Resume();
+        resume._name = "Connor Neus";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
     }
 }
