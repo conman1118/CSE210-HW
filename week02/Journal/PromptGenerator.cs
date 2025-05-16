2253b53977806b4bb1926e702b94e5dd122c094f
conman1118 <connor.f.neus@gmail.com>
2025-05-16 16:34:28 -0700

    done

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -1,13 +1,15 @@
 // PromptGenerator.cs
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace JournalApp
 {
-    // Provides random prompts
     public static class PromptGenerator
     {
-        private static readonly Random _rand = new Random();
+        private static readonly Random _rng = new Random();
+
+        // Your master list of prompts
         private static readonly List<string> _prompts = new List<string>
         {
             "Who was the most interesting person I interacted with today?",
@@ -15,13 +17,36 @@ namespace JournalApp
             "How did I see the hand of the Lord in my life today?",
             "What was the strongest emotion I felt today?",
             "If I had one thing I could do over today, what would it be?",
-            // feel free to add more of your own...
+            // …add any others you like
         };
 
+        // A working, shuffled copy
+        private static List<string> _shuffledPrompts;
+        private static int _nextIndex;
+
+        // Static constructor: prepare first shuffle
+        static PromptGenerator()
+        {
+            ResetShuffled();
+        }
+
+        // Public API: get the next prompt
         public static string GetRandomPrompt()
         {
-            int idx = _rand.Next(_prompts.Count);
-            return _prompts[idx];
+            // If we’ve run out, start a new round
+            if (_nextIndex >= _shuffledPrompts.Count)
+                ResetShuffled();
+
+            return _shuffledPrompts[_nextIndex++];
+        }
+
+        // Shuffle into _shuffledPrompts, reset index
+        private static void ResetShuffled()
+        {
+            _shuffledPrompts = _prompts
+                .OrderBy(_ => _rng.Next())
+                .ToList();
+            _nextIndex = 0;
         }
     }
 }
